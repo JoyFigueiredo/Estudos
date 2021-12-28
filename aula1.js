6abd26aff06c5138180fcae7c145c4c71a274a06
Joice Figueiredo <joicebf1995@outlook.com>
2021-12-28 18:37:24 -0300

    funçao

--- aula1.js
+++ aula1.js
@@ -1,7 +1,16 @@
+/*
 //declaraçao de variavel e constantes
-let mensagem = 'Ola mundo';
+let mensagem = 'Ola mundo'; //const, var... define o tipo da variavel automaticamente
 console.log(mensagem);
 mensagem = 'Oi';
-console.log(mensagem);
+console.log(typeof(mensagem));
+*/
 
-//
+//Função
+
+function somar(numA, numB){
+    return numA + numB;
+}
+
+let resultado = somar(10, 3);
+console.log(resultado);
